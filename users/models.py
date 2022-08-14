07090031103a6a610ce47bf7b63d0ac836f1fad4
Rafhael Mallorga <rafhaelmallorga@gmail.com>
2022-08-14 06:24:47 -0300

    feat :sparkles: created Users model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Seasons(models.TextChoices):
+    SUMMER = "Verão"
+    AUTUMN = "Outono"
+    WINTER = "Inverno"
+    SPRING = "Primavera"
+    DEFAULT = "Não especificado"
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=10)
+    last_name = models.CharField(max_length=10)
+    email = models.EmailField()
+    favorite_season = models.CharField(
+        max_length=20, choices=Seasons.choices, default=Seasons.DEFAULT
+    )
+
+    def __repr__(self) -> str:
+        return f"<{self.id} - {self.email}>"
