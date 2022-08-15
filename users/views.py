60de5ccae37e182aca8ad0dc541535ab5790f3ef
Rafhael Mallorga <rafhaelmallorga@gmail.com>
2022-08-15 15:28:25 -0300

    feat :sparkles: created users routes

--- users/views.py
+++ users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from types import ModuleType
 
-# Create your views here.
+from django.forms.models import model_to_dict
+from rest_framework.views import APIView, Request, Response, status
+
+from .models import User
+
+
+class UserView(APIView):
+    def get(self, request: Request):
+        users = User.objects.all()
+
+        users_list = [model_to_dict(user) for user in users]
+
+        return Response(users_list)
+
+    def post(self, request: Request):
+        ...
